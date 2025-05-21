863378096c58681ef29c45a3299b57e28080bab4
Perceptrix <jochemlange123@hotmail.com>
2025-05-21 14:43:47 +0200

    implemented cnn classifier and start of the harmonisation part

--- utils/methods_iradon.py
+++ utils/methods_iradon.py
@@ -3,7 +3,7 @@ import torch, deepinv as dinv
 from pathlib import Path
 import matplotlib.pyplot as plt
 from deepinv.optim import optim_builder
-from deepinv.optim.data_fidelity import L2
+from deepinv.optim.data_fidelity import L2, PoissonLikelihood
 from deepinv.optim.prior         import Tikhonov, TVPrior
 
 
@@ -28,7 +28,7 @@ def Algoritme_A(sinogram, physics, x_gt=None):
     data_fidelity = L2()
 
     # Specific parameters for restoration with the given prior (Note that these parameters have not been optimized here)
-    params_algo = {"stepsize": 0.0001, "lambda": 1e-2}
+    params_algo = {"stepsize": 0.0001, "lambda": 1e1}
 
     # Logging parameters
     verbose = True
@@ -39,7 +39,7 @@ def Algoritme_A(sinogram, physics, x_gt=None):
     # smaller than thres_conv
     thres_conv = 1e-5 # was 1e-5
     backtracking = False  # use backtraking to automatically adjust the stepsize
-    max_iter = 100  # Maximum number of iterations
+    max_iter = 1000  # Maximum number of iterations
 
     # Main algorithm
     model = optim_builder(
@@ -78,11 +78,14 @@ def Algoritme_B(sinogram, physics, x_gt=None):
     """
     # Load the sinogram
     y = sinogram
-    # prior_A = Tikhonov()
-    prior = TVPrior()
 
-    # Select the data fidelity term
-    data_fidelity = L2()
+    GAIN        = 1/40          # same as you used to corrupt the data
+    max_iter_B  = 500
+    # Prior
+    prior_B   = TVPrior()
+
+    # Data fidelity term
+    fidelityB = PoissonLikelihood(gain=GAIN,bkg=0.01, denormalize= True)
 
     # Specific parameters for restoration with the given prior (Note that these parameters have not been optimized here)
     params_algo = {"stepsize": 0.0001, "lambda": 1e1}
@@ -96,29 +99,26 @@ def Algoritme_B(sinogram, physics, x_gt=None):
     # smaller than thres_conv
     thres_conv = 1e-5 # was 1e-5
     backtracking = False  # use backtraking to automatically adjust the stepsize
-    max_iter = 100  # Maximum number of iterations
 
-    # Main algorithm
-    model = optim_builder(
+
+    model_B = optim_builder(
         iteration="GD",
-        prior=prior,
-        g_first=False,
-        data_fidelity=data_fidelity,
-        params_algo=params_algo,
+        prior=prior_B,
+        data_fidelity=fidelityB,
+        params_algo=params_algo,   
+        max_iter=max_iter_B,
         early_stop=early_stop,
-        max_iter=max_iter,
         crit_conv=crit_conv,
-        thres_conv=thres_conv,
-        backtracking=backtracking,
-        verbose=verbose,
-        custom_init=lambda y, physics: {  # initialization of the algorithm using fbp
+        thres_conv= thres_conv,
+        verbose=True,
+        custom_init=lambda y, physics: {
             "est": (physics.A_dagger(y), physics.A_dagger(y))
         }
     )
 
 
     # x_A = model(y, physics,x_gt = x_gt, compute_metric = True)
-    x_B, metrics = model(
+    x_B, metrics = model_B(
         y, physics, x_gt=x_gt, compute_metrics=True
     )  # reconstruction with PGD algorithm
 
